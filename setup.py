4e3e7866355f0f2d13d46e8e84b6dfab4366e544
Nitesh <niteshsoniofficial@gmail.com>
2021-11-03 23:18:15 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="pyhtmx",
+    version="0.0.1",
+    author="Nitesh",
+    author_email="niteshsoniofficial@gmail.com",
+    description="simple htmx in python",
+    long_description="pyhtmx",
+    long_description_content_type="text/markdown",
+    url="https://github.com/init-esh/pyhtmx",
+    project_urls={
+        "Bug Tracker": "https://github.com/init-esh/pyhtmx/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
